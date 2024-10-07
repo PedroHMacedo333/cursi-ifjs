3123ddfbc2a5b52879e0cfac72422b0fc22c0a5f
PedroHMacedo333 <pedromacedo1721@gmail.com>
2024-10-07 19:53:29 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const numero = 8;
+
+let multiplicador = 1;
+
+console.log(numero, "x", multiplicador, "=", numero * multiplicador++);
+console.log(numero, "x", multiplicador, "=", numero * multiplicador++);
+console.log(numero, "x", multiplicador, "=", numero * multiplicador++);
+console.log(numero, "x", multiplicador, "=", numero * multiplicador++);
+console.log(numero, "x", multiplicador, "=", numero * multiplicador++);
+console.log(numero, "x", multiplicador, "=", numero * multiplicador++);
+console.log(numero, "x", multiplicador, "=", numero * multiplicador++);
+console.log(numero, "x", multiplicador, "=", numero * multiplicador++);
+console.log(numero, "x", multiplicador, "=", numero * multiplicador++);
+console.log(numero, "x", multiplicador, "=", numero * multiplicador++);
+
+//tabuada S2
