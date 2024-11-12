48ef1c3f8fde8af96accbe8355cbd9193b5788d4
PedroHMacedo333 <pedromacedo1721@gmail.com>
2024-11-11 21:03:34 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
-// Solicite um número para o usuário, enquanto esse número for múltiplo de 5, continue pedindo outro número. Informem o que o número é múltiplo de 5.
+const readline = require('readline-sync')
 
-const readline = require('readline-sync');
+const nome = readline.question('Qual o seu Nome ? ')
+const idade = readline.questionInt('Qual a sua idade ? ')
 
-let valor = 0;
-// UAI Ô = Pronúncia da palavra WHILE
-while(valor % 5 == 0) {
-    valor = readline.questionInt("Informe um numero multiplo de 5: ");
-    if (valor % 5 == 0) {
-        console.log('Numero: ', valor);
-        console.log('É múltiplo de 5')
-    } else {
-        console.log('Numero: ', valor);
-        console.log('Não é múltiplo de 5')
-    }
+if (idade >= 18) {
+    console.log(`Parabens ${nome}Você maior de idade`)
+}else {
+    console.log(`Sinto muito mais Você ${nome}e menor de idade`)
 }
